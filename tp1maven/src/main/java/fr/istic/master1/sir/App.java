00a27500d439f060b00de63f10d2e576a765636c
julienmoulinrennes1 <julien.moulin35@gmail.com>
2015-01-13 15:57:31 +0100

    TP1Maven - review code

--- tp1maven/src/main/java/fr/istic/master1/sir/App.java
+++ tp1maven/src/main/java/fr/istic/master1/sir/App.java
@@ -4,10 +4,8 @@ package fr.istic.master1.sir;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
